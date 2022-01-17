9994cff2d061307142eee83e4ac5cdd264cce118
Levi Bostian <levi@levi.earth>
2022-01-17 16:53:13 -0600

    fix script

--- action.sh
+++ action.sh
@@ -1,13 +1,16 @@
+AHEAD="$1"
+BEHIND="$2"
+
 # if git switch unsuccessful, it's because the branch does not exist. exit 0 indicating that the action will simply ignore request. 
-git switch ${{ inputs.ahead }} || exit 0
+git switch $AHEAD || exit 0
 git pull 
-git switch ${{ inputs.behind }} || exit 0
+git switch $BEHIND || exit 0
 git pull 
 
-git rebase ${{ inputs.ahead }}
+git rebase $AHEAD
 
 # git log <branchX>..<branchY> gives you list of commits that are different between the two. If output empty, rebase was successful 
-if [[ $( git log ${{ inputs.behind }}..${{ inputs.ahead }} ) ]]; then 
+if [[ $( git log $BEHIND..$AHEAD ) ]]; then 
     echo "Rebase not successful. Commits not the same between the two branches"
     exit 1
 else 
