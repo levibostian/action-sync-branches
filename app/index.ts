8bcacc6e372755c110fbce9b5340ca6867d9d39b
Levi Bostian <levi@levi.earth>
2022-01-17 16:18:02 -0600

    deleted node related code

--- app/index.ts
+++
@@ -1,9 +0,0 @@
-import { getInput } from "./input"
-import * as git from "./git"
-;(async () => {
-  const input = getInput()
-
-  const repoUrl = await git.getRepoUrl(input.githubToken)
-  await git.verifyAuth(repoUrl)
-  await git.rebaseSyncBranches(input.behindBranchName, input.aheadBranchName, repoUrl)
-})()
