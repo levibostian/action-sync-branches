980dba084c66ad4770800e2b93063bb31e8ce327
Levi Bostian <levi@levi.earth>
2022-01-17 00:06:39 -0600

    fix: git checkout command

--- app/git/pull.ts
+++ app/git/pull.ts
@@ -1,6 +1,6 @@
 import { execCommand } from "./exec"
 
 export const checkoutAndPull = async (branch: string, repoUrl: string): Promise<void> => {
-  await execCommand(`git checkout ${branch}`, repoUrl)
+  await execCommand(`git checkout ${branch}`, "")
   await execCommand(`git pull ${branch}`, repoUrl)
 }
