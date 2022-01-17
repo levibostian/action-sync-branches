fe3a3427d1454ce732baaabc85fd478b1111a030
Levi Bostian <levi@levi.earth>
2022-01-17 00:01:23 -0600

    chage to using input

--- app/index.ts
+++ app/index.ts
@@ -1,17 +1,9 @@
 import { getInput } from "./input"
 import * as git from "./git"
-import { env } from "process"
 ;(async () => {
   const input = getInput()
 
-  const githubAuthToken = env["GITHUB_TOKEN"]
-  if (!githubAuthToken || githubAuthToken == "") {
-    throw new Error(
-      "Action needs to be able to authenticate with GitHub in order to run git commands. Read the documentation about authentication and try again."
-    )
-  }
-
-  const repoUrl = await git.getRepoUrl(githubAuthToken)
+  const repoUrl = await git.getRepoUrl(input.githubToken)
   await git.verifyAuth(repoUrl)
   await git.rebaseSyncBranches(input.behindBranchName, input.aheadBranchName, repoUrl)
 })()
--- app/input.ts
+++ app/input.ts
@@ -4,6 +4,7 @@ import * as core from "@actions/core"
 export const getInput = (): Input => {
   return {
     aheadBranchName: core.getInput("ahead"),
-    behindBranchName: core.getInput("behind")
+    behindBranchName: core.getInput("behind"),
+    githubToken: core.getInput("token")
   }
 }
